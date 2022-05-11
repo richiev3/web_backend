346bd0f4b5be937547cd481514e3201aa603ec4e
richard vazquez <richard.vazquez63@myhunter.cuny.edu>
2022-05-11 17:00:58 -0400

    starter code

---
+++ database/models/Student.js
@@ -0,0 +1,22 @@
+/*==================================================
+/database/database/models/Student.js
+
+It defines the student model for the database.
+==================================================*/
+const Sequelize = require('sequelize');  // Import Sequelize
+const db = require('../db');  // Import Sequelize database instance called "db"
+
+const Student = db.define("student", {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Export the student model
+module.exports = Student;
