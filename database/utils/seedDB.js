2f8ec8aec43ab2091e8eeaac4d4a302afcffed1d
richard vazquez <richard.vazquez63@myhunter.cuny.edu>
2022-05-13 14:47:21 -0400

    had to reclone starter code bc of npm issue

---
+++ database/utils/seedDB.js
@@ -0,0 +1,51 @@
+/*==================================================
+/database/utils/seedDB.js
+
+It seeds the database with several initial students and campuses.
+==================================================*/
+const { Campus, Student } = require('../models');  // Import database models
+
+// Seed database
+const seedDB = async () => {
+	// Create a new campus
+	const dummy_campus = await Campus.create({
+		name: "Hunter College",
+		address: "695 Park Ave, New York, NY 10065",
+		description: "This is a school in New York, New York."
+	});
+	// Create a new campus
+	const dummy_campus2 = await Campus.create({
+		name: "Queens College",
+		address: "65-30 Kissena Blvd, Queens, NY 11367",
+		description: "This is a school in Queens, New York."
+	});
+	// Create a new campus
+	const dummy_campus3 = await Campus.create({
+		name: "Brooklyn College",
+		address: "2900 Bedford Ave, Brooklyn, NY 11210",
+		description: "This is a school in Brooklyn, New York."
+	});
+	
+	// Create a new student for a campus
+	const dummy_student = await Student.create({
+		firstname: "Joe",
+      lastname: "Smith",
+		email: "joesmith@gmail.com",
+		gpa: 3.5
+	});
+	// Create a new student for a campus
+	const dummy_student2 = await Student.create({
+		firstname: "Mary",
+      lastname: "Johnson",
+		email: "maryjohnson@gmail.com",
+		gpa: 2.1
+	});
+
+	// Add students to campuses
+	await dummy_student.setCampus(dummy_campus);
+	await dummy_student2.setCampus(dummy_campus2);
+	await dummy_student2.setCampus(dummy_campus3);
+}
+
+// Export the database seeding function
+module.exports = seedDB;
